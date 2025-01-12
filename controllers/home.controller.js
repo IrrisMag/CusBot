0f13412fd8ac3eddff3dbca8f34693a4c883e766
IrrisMag <magneirris@gmail.com>
2025-01-12 22:05:01 +0100

    CusBot 1.1.1

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,6 +1,6 @@
-const Product = require('../lib/models/product.model.js'); // Assuming you have a Product model
+const Product = require('../lib/models/product.model.js');
 
-exports.showHome = async (req, res) => {
+const showHome = async (req, res) => {
   try {
     const products = await Product.find(); // Fetch products from the database
     res.render('pages/home', { title: 'Customers', products }); // Pass products to the template
@@ -9,3 +9,24 @@ exports.showHome = async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 };
+
+const showBot = async (req, res) => {
+
+    const chatHistory = []; // Initialize empty or fetch from a database
+    const faqs = [
+        { question: 'How do I track my order?', answer: 'You can track your order by clicking on "Track Order" button above.' },
+        { question: 'How do I reset my password?', answer: 'Click on "Account Info" and then select "Reset Password".' },
+    ]; // Fetch FAQs from a database or static list
+    const languages = [
+        { code: 'en', name: 'English', selected: true },
+        { code: 'es', name: 'Spanish', selected: false },
+        { code: 'fr', name: 'French', selected: false },
+    ];
+
+    res.render('pages/bot', { title: 'Chat Support', chatHistory, faqs, languages });
+  }
+
+module.exports = {
+    showHome,
+    showBot,
+  };
--- routes/home.route.js
+++ routes/home.route.js
@@ -3,25 +3,11 @@ const router = express.Router();
 
 
 
-const { showHome } = require('../controllers/home.controller');
+const { showHome, showBot } = require('../controllers/home.controller');
 
 router.get('/', showHome);
-// In your Express app setup
 
-router.get('/bot', (req, res) => {
-    const chatHistory = []; // Initialize empty or fetch from a database
-    const faqs = [
-        { question: 'How do I track my order?', answer: 'You can track your order by clicking on "Track Order" button above.' },
-        { question: 'How do I reset my password?', answer: 'Click on "Account Info" and then select "Reset Password".' },
-    ]; // Fetch FAQs from a database or static list
-    const languages = [
-        { code: 'en', name: 'English', selected: true },
-        { code: 'es', name: 'Spanish', selected: false },
-        { code: 'fr', name: 'French', selected: false },
-    ];
-
-    res.render('pages/bot', { title: 'Chat Support', chatHistory, faqs, languages });
-});
+router.get('/bot', showBot);
 
 
 
